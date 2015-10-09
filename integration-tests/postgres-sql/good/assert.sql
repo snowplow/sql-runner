95c4b6e4237bdb957c86b77642d66e39e8ef860c
Alessandro Andrioni <alessandroandrioni@gmail.com>
2015-10-09 13:09:31 -0300

    Make Travis happy again

--- integration-tests/postgres-sql/good/assert.sql
+++ integration-tests/postgres-sql/good/assert.sql
@@ -7,9 +7,9 @@ DECLARE
   expected_average_age CONSTANT integer := 25;
 BEGIN
   {{/* Update view names to today's date or Travis will error */}}
-  IF (SELECT average_age <> expected_average_age FROM {{.test_schema}}.view_2015_09_13) THEN
+  IF (SELECT average_age <> expected_average_age FROM {{.test_schema}}.view_2015_10_09) THEN
     RAISE EXCEPTION 'Average_age % does not match expected age %',
-    	(SELECT average_age FROM {{.test_schema}}.view_2015_09_13),
+    	(SELECT average_age FROM {{.test_schema}}.view_2015_10_09),
     	expected_average_age;
   END IF;
 END;
