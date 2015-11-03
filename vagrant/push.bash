96086a0a3aa7c6dc7e1b861ca7b88a5ed63a1c93
Josh <joshua.a.beemster@gmail.com>
2015-11-03 10:02:52 +0000

    Upgrade vagrant push to upload OS X and Windows binaries (closes #31)

--- vagrant/push.bash
+++ vagrant/push.bash
@@ -17,11 +17,42 @@ cd ${dir}
 # Version is stored in a file
 version=`cat VERSION`
 
-# Zip the artifact (assumes godep go build already run)
-hyphenated_package=`echo ${bintray_package}|tr '-' '_'`
-artifact="${hyphenated_package}_${version}_linux_amd64.zip"
-rm -f ${artifact}
-zip ${artifact} ${bintray_package}
+# Build Windows, OSX and Linux variations
+build_dir="/opt/gopath/src/github.com/snowplow/sql-runner"
+dist_dir="/vagrant/dist"
+build_cmd="godep go build"
+goarch_64="amd64"
+
+# 1: Windows
+echo "============================="
+echo "  Building Windows artifact  "
+echo "-----------------------------"
+goos_win="windows"
+vagrant ssh -c "cd ${build_dir} && export GOOS=${goos_win} && export GOARCH=${goarch_64} && ${build_cmd} -o ${dist_dir}/${bintray_package}.exe"
+artifact_win="sql_runner_${version}_${goos_win}_${goarch_64}.zip"
+zip dist/${artifact_win} dist/${bintray_package}.exe
+
+# 2: Mac OSX
+echo "========================="
+echo "  Building OSX artifact  "
+echo "-------------------------"
+goos_osx="darwin"
+vagrant ssh -c "cd ${build_dir} export GOOS=${goos_osx} && export GOARCH=${goarch_64} && ${build_cmd} -o ${dist_dir}/${bintray_package}"
+artifact_osx="sql_runner_${version}_${goos_osx}_${goarch_64}.zip"
+zip dist/${artifact_osx} dist/${bintray_package}
+
+# 3: Linux
+echo "==========================="
+echo "  Building Linux artifact  "
+echo "---------------------------"
+goos_linux="linux"
+vagrant ssh -c "cd ${build_dir} export GOOS=${goos_linux} && export GOARCH=${goarch_64} && ${build_cmd} -o ${dist_dir}/${bintray_package}"
+artifact_linux="sql_runner_${version}_${goos_linux}_${goarch_64}.zip"
+zip dist/${artifact_linux} dist/${bintray_package}
+
+echo "=================================="
+echo "  Uploading artifacts to bintray  "
+echo "----------------------------------"
 
 # Create the version (does nothing if already exists)
 echo '{"name":"'${version}'","desc":"Release of '${bintray_package}'"}' | curl -d @- \
@@ -30,6 +61,14 @@ echo '{"name":"'${version}'","desc":"Release of '${bintray_package}'"}' | curl -
 -u${bintray_user}:${bintray_api_key} \
 
 # Upload the artifact (overwrites if already exists)
-curl -T ${artifact} \
-"https://api.bintray.com/content/${bintray_repository}/${bintray_package}/${version}/${artifact}?publish=1&override=1" \
+curl -T dist/${artifact_win} \
+"https://api.bintray.com/content/${bintray_repository}/${bintray_package}/${version}/${artifact_win}?publish=1&override=1" \
+-u${bintray_user}:${bintray_api_key} \
+
+curl -T dist/${artifact_osx} \
+"https://api.bintray.com/content/${bintray_repository}/${bintray_package}/${version}/${artifact_osx}?publish=1&override=1" \
+-u${bintray_user}:${bintray_api_key} \
+
+curl -T dist/${artifact_linux} \
+"https://api.bintray.com/content/${bintray_repository}/${bintray_package}/${version}/${artifact_linux}?publish=1&override=1" \
 -u${bintray_user}:${bintray_api_key} \
