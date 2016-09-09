48c4a0ad460d96b5ab1012cedd7f26afd946b151
Josh <joshua.a.beemster@gmail.com>
2016-09-09 18:09:13 +0200

    Reset Godeps

--- Godeps/_workspace/src/gopkg.in/pg.v3/db.go
+++ Godeps/_workspace/src/gopkg.in/pg.v3/db.go
@@ -149,14 +149,8 @@ func (opt *Options) getSSL() bool {
 // and maintains its own connection pool.
 func Connect(opt *Options) *DB {
 	return &DB{
-		opt: opt,
-		pool: newConnPool(&connPoolOptions{
-			Dialer:             newConnDialer(opt),
-			PoolSize:           opt.getPoolSize(),
-			PoolTimeout:        opt.getPoolTimeout(),
-			IdleTimeout:        opt.getIdleTimeout(),
-			IdleCheckFrequency: opt.getIdleCheckFrequency(),
-		}),
+		opt:  opt,
+		pool: newConnPool(opt),
 	}
 }
 
@@ -185,7 +179,7 @@ func (db *DB) WithTimeout(d time.Duration) *DB {
 }
 
 func (db *DB) conn() (*conn, error) {
-	cn, err := db.pool.Get()
+	cn, _, err := db.pool.Get()
 	if err != nil {
 		return nil, err
 	}
@@ -199,8 +193,8 @@ func (db *DB) freeConn(cn *conn, err error) error {
 	if err == nil {
 		return db.pool.Put(cn)
 	}
-	if cn.br.Buffered() > 0 {
-		return db.pool.Remove(cn)
+	if cn.rd.Buffered() > 0 {
+		return db.pool.Remove(cn, err)
 	}
 	if pgerr, ok := err.(Error); ok && pgerr.Field('S') != "FATAL" {
 		return db.pool.Put(cn)
@@ -213,7 +207,7 @@ func (db *DB) freeConn(cn *conn, err error) error {
 			log.Printf("pg: cancelRequest failed: %s", err)
 		}
 	}
-	return db.pool.Remove(cn)
+	return db.pool.Remove(cn, err)
 }
 
 // Close closes the database client, releasing any open resources.
@@ -313,64 +307,11 @@ func (db *DB) CopyFrom(r io.Reader, q string, args ...interface{}) (Result, erro
 	if err != nil {
 		return nil, err
 	}
-
-	if err := writeQueryMsg(cn.buf, q, args...); err != nil {
-		db.pool.Put(cn)
-		return nil, err
-	}
-
-	if err := cn.FlushWrite(); err != nil {
-		db.freeConn(cn, err)
-		return nil, err
-	}
-
-	if err := readCopyInResponse(cn); err != nil {
-		db.freeConn(cn, err)
-		return nil, err
-	}
-
-	ready := make(chan struct{})
-	var res Result
-	go func() {
-		res, err = readReadyForQuery(cn)
-		close(ready)
-	}()
-
-	for {
-		select {
-		case <-ready:
-			break
-		default:
-		}
-
-		_, err := writeCopyData(cn.buf, r)
-		if err == io.EOF {
-			break
-		}
-
-		if err := cn.FlushWrite(); err != nil {
-			db.freeConn(cn, err)
-			return nil, err
-		}
-	}
-
-	select {
-	case <-ready:
-	default:
-	}
-
-	writeCopyDone(cn.buf)
-	if err := cn.FlushWrite(); err != nil {
-		db.freeConn(cn, err)
-		return nil, err
-	}
-
-	<-ready
+	res, err := copyFrom(cn, r, q, args...)
 	if err != nil {
 		db.freeConn(cn, err)
 		return nil, err
 	}
-
 	db.pool.Put(cn)
 	return res, nil
 }
@@ -485,6 +426,61 @@ func assertOneAffected(res Result, coll *singleRecordCollection) (Result, error)
 		if err := assertOne(coll.Len()); err != nil {
 			return nil, err
 		}
+	}
+	return res, nil
+}
+
+func copyFrom(cn *conn, r io.Reader, q string, args ...interface{}) (Result, error) {
+	if err := writeQueryMsg(cn.buf, q, args...); err != nil {
+		return nil, err
+	}
+
+	if err := cn.FlushWrite(); err != nil {
+		return nil, err
+	}
+
+	if err := readCopyInResponse(cn); err != nil {
+		return nil, err
+	}
+
+	ready := make(chan struct{})
+	var res Result
+	var err error
+	go func() {
+		res, err = readReadyForQuery(cn)
+		close(ready)
+	}()
+
+	for {
+		select {
+		case <-ready:
+			break
+		default:
+		}
+
+		_, err := writeCopyData(cn.buf, r)
+		if err == io.EOF {
+			break
+		}
+
+		if err := cn.FlushWrite(); err != nil {
+			return nil, err
+		}
+	}
+
+	select {
+	case <-ready:
+	default:
+	}
+
+	writeCopyDone(cn.buf)
+	if err := cn.FlushWrite(); err != nil {
+		return nil, err
+	}
+
+	<-ready
+	if err != nil {
+		return nil, err
 	}
 	return res, nil
 }
