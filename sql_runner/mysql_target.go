c72e53d053241fef03f17b3f38614068ac8be98f
Eric Su <eric-cc-su@users.noreply.github.com>
2021-01-24 17:13:25 -0500

    SELECT rows affected handling and printTable

--- sql_runner/mysql_target.go
+++ sql_runner/mysql_target.go
@@ -11,6 +11,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 const msNetwork = "tcp"
@@ -88,6 +89,14 @@ func (mt MySQLTarget) GetTarget() Target {
 	return mt.Target
 }
 
+// Check if query is a SELECT by checking the starting SQL command.
+// This should ensure that we return an accurate Rows Affected value for SELECT queries to match postgres_target's Rows Affected policy
+func (query ReadyQuery) isSelectQuery() bool {
+	firstWord := strings.ToLower(strings.Split(query.Script, " ")[0])
+
+	return firstWord == "select"
+}
+
 // Run a query against the target
 func (mt MySQLTarget) RunQuery(query ReadyQuery, dryRun bool, showQueryOutput bool) QueryStatus {
 	var err error = nil
@@ -102,15 +111,12 @@ func (mt MySQLTarget) RunQuery(query ReadyQuery, dryRun bool, showQueryOutput bo
 	}
 
 	var affected int64 = 0
-	if showQueryOutput {
-		var rows *sql.Rows
+
+	if query.isSelectQuery() {
 		rows, err := mt.Client.Query(query.Script)
-		if err != nil {
-			log.Printf("ERROR: %s.", err)
-			return QueryStatus{query, query.Path, affected, err}
+		if err == nil {
+			affected, _ = interpretRows(rows, showQueryOutput)
 		}
-		// TODO: Implement printTable
-		// affected, _ = printTable(rows)
 	} else {
 		res, err := mt.Client.Exec(query.Script)
 		if err == nil {
@@ -119,4 +125,63 @@ func (mt MySQLTarget) RunQuery(query ReadyQuery, dryRun bool, showQueryOutput bo
 	}
 
 	return QueryStatus{query, query.Path, affected, err}
+}
+
+// Convert slice of string pointers to slice of strings to be used with tablewriter
+func stringList(spList []*string) []string {
+	sList := make([]string, len(spList))
+	for i := range spList {
+		if spList[i] != nil {
+			sList[i] = *spList[i]
+		} else {
+			sList[i] = "NULL"
+		}
+	}
+	return sList
+}
+
+// Interpret sql.Rows results to ensure all rows are counted
+func interpretRows(rows *sql.Rows, shouldPrintTable bool) (affected int64, funcErr error) {
+	defer rows.Close()
+
+	if !shouldPrintTable {
+		for rows.Next() {
+			affected += 1
+		}
+	} else {
+		affected, funcErr = printTable(rows)
+	}
+	return
+}
+
+// Print table produced by sql.DB.Query
+func printTable(rows *sql.Rows) (affected int64, funcErr error) {
+	columns, colErr := rows.Columns()
+	if colErr != nil {
+		return affected, errors.New("Unable to read columns")
+		log.Printf("ERROR | printTable: %s", colErr)
+	}
+
+	log.Printf("QUERY OUTPUT:\n")
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader(columns)
+	table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
+	table.SetCenterSeparator("|")
+
+	strs := make([]*string, len(columns))
+	vals := make([]interface{}, len(columns))
+	for rows.Next() {
+		for i := range vals {
+			vals[i] = &strs[i]
+		}
+		if err := rows.Scan(vals...); err != nil {
+			log.Printf("ERROR | printTable: %s", err)
+		} else {
+			affected += 1
+			table.Append(stringList(strs))
+		}
+	}
+
+	table.Render()
+	return
 }
