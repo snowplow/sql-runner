66b395dab94b3e53ae5aa6947e5cdc05518c8dd2
jbeemster <joshua.a.beemster@gmail.com>
2025-05-13 15:15:02 +0200

    Format and update COPYRIGHT

--- sql_runner/provider.go
+++ sql_runner/provider.go
@@ -1,5 +1,4 @@
-//
-// Copyright (c) 2015-2022 Snowplow Analytics Ltd. All rights reserved.
+// Copyright (c) 2015-2025 Snowplow Analytics Ltd. All rights reserved.
 //
 // This program is licensed to you under the Apache License Version 2.0,
 // and you may not use this file except in compliance with the Apache License Version 2.0.
@@ -9,7 +8,6 @@
 // software distributed under the Apache License Version 2.0 is distributed on an
 // "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
-//
 package main
 
 // PlaybookProvider is an interface for different providers.
